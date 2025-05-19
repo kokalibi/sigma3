d4f1b2506e4f277677f8df61ce8ec40ef029c028
kadam1222 <k.adam12220@gmail.com>
2025-05-19 08:30:06 +0200

    asd

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,13 +5,24 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DotNetEnv;
 
 namespace ugyfel
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+    
     public partial class App : Application
     {
+        public static string ConnString { get; private set; }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // .env betöltése
+            DotNetEnv.Env.Load();
+            ConnString = $"server={Env.GetString("DB_HOST")};user={Env.GetString("DB_USER")};password={Env.GetString("DB_PASS")};database={Env.GetString("DB_NAME")}";
+            //ablak
+            Regisztracio ablak = new Regisztracio();
+            MainWindow = ablak;
+            ablak.Show();
+        }
     }
 }
---
+++ Regisztracio.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ugyfel
+{
+    /// <summary>
+    /// Interaction logic for Regisztracio.xaml
+    /// </summary>
+    public partial class Regisztracio : Window
+    {
+        public Regisztracio()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string nev = felhasznalonev.Text;
+            string pass = jelszo.Password;
+            string passujra = jelszoujra.Password;
+            string emailcim = email.Text;
+
+            if(string.IsNullOrEmpty(nev) || string.IsNullOrEmpty(pass))
+            {
+                MessageBox.Show("Töltsön ki minden mezőt!", "Hiányzó adat!" ,MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+    }
+}
