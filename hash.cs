ac223a25537d928ea6b687168b6632c60998177a
kokalibi <balint00istvan@gmail.com>
2025-06-03 10:32:17 +0200

    :P

---
+++ hash.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ugyfel
+{
+    internal class hash
+    {
+        public static string KeszitHash(string jelszo)
+        {
+            string kodolt="";
+            byte[] salt=new byte[16];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+                byte[] jelszoByte = Encoding.UTF8.GetBytes(jelszo);
+                byte[] Combined = new byte[salt.Length+jelszoByte.Length];
+                Buffer.BlockCopy(salt,0,Combined,0, salt.Length);
+                Buffer.BlockCopy(jelszoByte,0,Combined,salt.Length,jelszoByte.Length);
+            }
+            return  kodolt;
+        }
+    }
+}
