4a5e5612eca076d3e426c321ca60bdc6fd5659f2
kokalibi <balint00istvan@gmail.com>
2025-06-03 09:15:36 +0200

    :P

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using DotNetEnv;
 
+
 namespace ugyfel
 {
     
--- Regisztracio.xaml.cs
+++ Regisztracio.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+
 
 namespace ugyfel
 {
@@ -36,7 +38,37 @@ namespace ugyfel
                 MessageBox.Show("Töltsön ki minden mezőt!", "Hiányzó adat!" ,MessageBoxButton.OK,MessageBoxImage.Error);
                 return;
             }
-            if()
+            if (pass != passujra)
+            {
+                MessageBox.Show("Nem ugyan az","Nem jo",MessageBoxButton.OK ,MessageBoxImage.Error);
+                return;
+            }
+            if(!emailcim.Contains("@")|| !emailcim.Contains("."))
+            {
+                MessageBox.Show("Nem email", "Nem jo", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                using (MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection(App.ConnString))
+                {
+                    conn.Open();
+                    string sql = "select count(*) from felhasznalo where felhasznalo=@fnev or email=@emil";
+                    var check=new MySql.Data.MySqlClient.MySqlCommand(sql, conn);
+                    check.Parameters.AddWithValue("@fnev", nev);
+                    check.Parameters.AddWithValue("@emil", emailcim);
+                    int db = (int)check.ExecuteScalar();
+                    if (db>0)
+                    {
+                        MessageBox.Show("mar letezik", "reg hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("adatbazis hiba:\n" + ex.Message, "adat");
+            }
         }
         
 
